04bcb387fb5889df586acc52b76ad98b994bd09d
NiflheimZ-P <siwaphat.gundej@gmail.com>
2023-11-10 18:00:50 +0700

    Add Action

--- Mappa/Mappa/auth.py
+++ Mappa/Mappa/auth.py
@@ -11,20 +11,20 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         username =  request.form.get('username')
         password = request.form.get('password')
 
-        if len(email) < 4:
-            flash('Email must be greater than 4 characters.', category='error')
-        elif len(username) < 2:
-            flash('Username must be greater than 3 characters.', category='error')
-        elif len(password) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            flash('Account created!', category='success')
+        # if len(email) < 4:
+        #     flash('Email must be greater than 4 characters.', category='error')
+        # elif len(username) < 2:
+        #     flash('Username must be greater than 3 characters.', category='error')
+        # elif len(password) < 7:
+        #     flash('Password must be at least 7 characters.', category='error')
+        # else:
+        #     flash('Account created!', category='success')
             
     return render_template("")
